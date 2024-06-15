616d36be383110b8dcba7885b64f4261d4c138fc
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-15 15:13:53 +0000

    refactor: Update DatabaseConnection package name and add connection details

--- src/Utils/DatabaseConnection.java
+++ src/Utils/DatabaseConnection.java
@@ -1,5 +1,21 @@
-package Utils;
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/pharmacy_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
