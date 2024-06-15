6dbb93408e2ee6998e46988d16c397fc19c05c00
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-15 15:23:50 +0000

    refactor: Update package name for DatabaseConnection class

--- src/Utils/DatabaseConnection.java
+++ src/Utils/DatabaseConnection.java
@@ -3,6 +3,7 @@ package Utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+//  The DatabaseConnection class provides a static method getConnection() that returns a Connection object to the database.
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/pharmacy_db";
