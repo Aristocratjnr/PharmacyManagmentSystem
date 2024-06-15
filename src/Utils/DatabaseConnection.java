9e86cc298819f384eda2b828f6746e6c103eb282
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-15 15:26:19 +0000

    refactor: Update package name for DatabaseConnection class with comments

--- src/Utils/DatabaseConnection.java
+++ src/Utils/DatabaseConnection.java
@@ -1,11 +1,12 @@
 package Utils;
+// We will need to provide the correct URL, username, and password to connect to the database.
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 //  The DatabaseConnection class provides a static method getConnection() that returns a Connection object to the database.
-// This class uses the DriverManager class to establish a connection to the MySQL database and it hasn't been setup yet.
 
+// This class uses the DriverManager class to establish a connection to the MySQL database and it hasn't been setup yet.
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/pharmacy_db";
