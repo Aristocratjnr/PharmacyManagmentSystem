8dc72de764d46cd612796b695c6f033315a216eb
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-15 15:12:53 +0000

    Refactor code to use lowercase package name and update imports

--- src/Main/PharmacyManagementSystem.java
+++ src/Main/PharmacyManagementSystem.java
@@ -1,5 +1,41 @@
-package Main;
+package main;
+
+import models.Drug;
+import models.Supplier;
+import utils.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 public class PharmacyManagementSystem {
-    
+    private HashMap<String, Drug> drugs;
+    private Connection connection;
+
+    public PharmacyManagementSystem() {
+        this.drugs = new HashMap<>();
+        this.connection = DatabaseConnection.getConnection();
+    }
+
+    public void addDrug(Drug drug) {
+        drugs.put(drug.getDrugCode(), drug);
+        String sql = "INSERT INTO Drugs (drugCode, name, description, stock, price) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, drug.getDrugCode());
+            pstmt.setString(2, drug.getName());
+            pstmt.setString(3, drug.getDescription());
+            pstmt.setInt(4, drug.getStock());
+            pstmt.setDouble(5, drug.getPrice());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Drug searchDrug(String drugCode) {
+        return drugs.get(drugCode);
+    }
+
+    // Other methods (viewAllDrugs, viewPurchaseHistory, etc.)
 }
